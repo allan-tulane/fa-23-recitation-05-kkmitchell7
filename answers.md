f0daf939161bbe48eb25aefc34c7cc1955e48f09
kkmitchell7 <kailen53.mitchell@gmail.com>
2023-11-01 20:34:54 -0500

    Final lab 5

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 6
 ## Answers
 
-**Name:**_________________________
+**Name:** Kailen Mitchell
 
 
 Place all written answers from `recitation-06.md` here for easier grading.
@@ -10,17 +10,34 @@ Place all written answers from `recitation-06.md` here for easier grading.
 
 - **d.**
 
-File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
+File | Fixed-Length Coding | Huffman Coding | Fixed-Length/Huffman
 ----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
+f1.txt        |        1340        |       826         | 1.62
+alice29.txt   |        1039367     |      676374       | 1.54
+asyoulik.txt  |        876253      |      606448       | 1.44
+grammar.lsp   |        26047       |      17356        | 1.50
+fields.c      |         78050      |      56206        | 1.39
 
+Do you see a consistent trend? If so, what is it?
+
+The ratio is around 1.6-1.4
+The ratio seems to be decreasing slightly but that could be a coincidence. 
 
 
 
 - **e.**
 
+  For each chracter, the Huffman cost is length of huffman encoding * the frequency of that character
+  Since we would get a balanced tree with frequencies that are all the same, the length of the encoding
+  would start with 1 and would increase by 1 for each level deep we go into the tree.
+  
+  From 1 to log(n) is the number of levels
+  
+  For each level the length of encoding is increasing by 1
+  
+  For each level the number of nodes is increasing by *2 for each node or 2*d-1 nodes total where d is the depth
 
+
+  cost = $\Sigma$ (from 1-log(x)) : n * F * 2*(log(n))-1
+  
+  where x is the number of terms and F is the frequency constant
--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def get_frequencies(fname):
 # create a prefix code tree using Huffman's algorithm
 def make_huffman_tree(f):
     p = queue.PriorityQueue()
+    
     # construct heap from frequencies, the initial items should be
     # the leaves of the final tree
     for c in f.keys():
@@ -34,27 +35,55 @@ def make_huffman_tree(f):
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
+        x = p.get()
+        y = p.get()
+        z = TreeNode(x,y,(x.data[0]+y.data[0],""))
+        p.put(z)
         
     # return root of the tree
+
     return p.get()
+
+"""
+Complete the implementation of Huffman coding in
+  `make_huffman_tree`. Note that we manipulate binary trees in the
+  priority queue using the object `TreeNode`. Moreover, once the tree
+  is constructed, we must compute the actual encodings by traversing
+  the Huffman tree that has been constructed. To do this, complete the
+  implementation of `get_code`, which is a typical recursive binary
+  tree traversal. That is, given a tree node, we recursively visit the
+  left and right subtrees, appending a `0` or `1` to the encoding in
+  each direction as appropriate. If we visit a leaf of the tree (which
+  represents a character in the alphabet) we store the
+  collected encoding for that character in `code`.
+
+"""
 
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+    if node.left != None:
+        get_code(node.left, prefix + "0", code)
+    if node.right != None:
+        get_code(node.right, prefix + "1", code)
+    if node.left == None and node.right == None:
+        code[node.data[1]] = prefix
+    return code
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
-
+    #  Compute cost for a fixed length encoding for each text file by calling function get_frequencies
+    return sum(f.values()) * math.ceil(math.log(len(f),2))
+ 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
-    # TODO
-    pass
+    cost = 0
+    for key in f:
+        #f is a dictionary of letters mapped to frequencies
+        #C is a distionary of nodes mapped to length of encodings
+        cost += f[key] * len(C[key]) #for each item, length of encoding * the frequency of that character
+    return cost
 
-f = get_frequencies('f1.txt')
+f = get_frequencies('fields.c')
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
 T = make_huffman_tree(f)
 C = get_code(T)
